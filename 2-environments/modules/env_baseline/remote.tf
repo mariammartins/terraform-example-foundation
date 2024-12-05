3bccb7f70ea0f2944888f81f9698dbf425d12d69
mariammartins <mariamartins@ciandt.com>
2024-12-05 16:11:25 -0300

    rm restricted references

--- 2-environments/modules/env_baseline/remote.tf
+++ 2-environments/modules/env_baseline/remote.tf
@@ -23,7 +23,7 @@ locals {
   tags                             = data.terraform_remote_state.org.outputs.tags
   required_groups                  = data.terraform_remote_state.bootstrap.outputs.required_groups
   enforce_vpcsc                    = data.terraform_remote_state.org.outputs.enforce_vpcsc
-  perimeter_name                   = data.terraform_remote_state.org.outputs.restricted_service_perimeter_name
+  perimeter_name                   = data.terraform_remote_state.org.outputs.service_perimeter_name
   access_context_manager_policy_id = data.terraform_remote_state.org.outputs.access_context_manager_policy_id
 }
 
