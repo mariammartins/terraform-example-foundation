9cb6ce042c2b39d4e5762b199535496663e80b8e
mariammartins <mariamartins@ciandt.com>
2024-12-05 15:08:59 -0300

    rm restricted name hub and spoke resources

--- 3-networks-hub-and-spoke/envs/development/outputs.tf
+++ 3-networks-hub-and-spoke/envs/development/outputs.tf
@@ -24,33 +24,33 @@ output "host_project_id" {
   description = "The host project ID"
 }
 
-output "restricted_network_name" {
-  value       = module.base_env.restricted_network_name
+output "network_name" {
+  value       = module.base_env.network_name
   description = "The name of the VPC being created"
 }
 
-output "restricted_network_self_link" {
-  value       = module.base_env.restricted_network_self_link
+output "network_self_link" {
+  value       = module.base_env.network_self_link
   description = "The URI of the VPC being created"
 }
 
-output "restricted_subnets_names" {
-  value       = module.base_env.restricted_subnets_names
+output "subnets_names" {
+  value       = module.base_env.subnets_names
   description = "The names of the subnets being created"
 }
 
-output "restricted_subnets_ips" {
-  value       = module.base_env.restricted_subnets_ips
+output "subnets_ips" {
+  value       = module.base_env.subnets_ips
   description = "The IPs and CIDRs of the subnets being created"
 }
 
-output "restricted_subnets_self_links" {
-  value       = module.base_env.restricted_subnets_self_links
+output "subnets_self_links" {
+  value       = module.base_env.subnets_self_links
   description = "The self-links of subnets being created"
 }
 
-output "restricted_subnets_secondary_ranges" {
-  value       = module.base_env.restricted_subnets_secondary_ranges
+output "subnets_secondary_ranges" {
+  value       = module.base_env.subnets_secondary_ranges
   description = "The secondary ranges associated with these subnets"
 }
 
